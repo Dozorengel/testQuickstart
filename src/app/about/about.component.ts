a1bbbb7355208d4e45ca95d18d733e1c5681e6f0
Dozorengel <dozorengel@gmail.com>
2018-06-22 02:25:36 +0800

    trying to do data binding between components

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { AppService } from './../app.service';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html',
+  template: `Say {{userName}}`,
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService) { }
+
+  pricelist: any;
+  result: string = 'Hello';
+  _userName: string;
 
   ngOnInit() {
+    this.appService.getPriceList().subscribe(d => {
+      this.pricelist = d.pricelist;
+    });
   }
 
+  @Input()
+  set userName(age: string) {
+    this._userName = age;
+  }
+  get userName() { return this._userName; }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
 import { AppService } from './app.service';
-
 import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html'
 })
 
-export class AppComponent implements OnInit  {
-  constructor(private appService: AppService) {}
+export class AppComponent implements OnInit {
+  constructor(private appService: AppService) { 
+    // this.text = 'Other World';
+  }
 
   pricelist: any;
+  name:string="Tom";
 
   ngOnInit() {
     this.appService.getPriceList().subscribe(d => {
@@ -22,6 +23,16 @@ export class AppComponent implements OnInit  {
 
   getDynamicInput() {
     return AboutComponent;
+  }
+
+  mouseEnter(div: string) {
+    console.log("mouse enter : " + div);
+    // this.text = 'Enter';
+  }
+
+  mouseLeave(div: string) {
+    console.log('mouse leave :' + div);
+    // this.text = 'Leave';
   }
 
 }
